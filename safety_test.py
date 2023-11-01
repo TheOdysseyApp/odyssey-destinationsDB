6d427d8f00e96fa85942e217c5c6cadb2135e443
Vanessa Tang <vanesst5@uci.edu>
2023-10-31 20:46:04 -0700

    Wrote script for safety data ------------------------ wrote general script to call the api based on the longitude and latitude of the items inside the db and updates the item with safety info

--- safety_test.py
+++ safety_test.py
@@ -11,6 +11,7 @@ def main():
 
     client = create_client(service_name="dynamodb", region_name="us-east-1")
     table_name = "travel-destination"
+    amadeus_client = create_amadeus_client()
 
     try:
         response = client.scan(TableName=table_name)
@@ -18,59 +19,98 @@ def main():
             items = response['Items']
             # Process each item
             for item in items:
-                print(f"{item}")
+                city = item["city"]["S"]
+                latitude = item["latitude"]["N"]
+                longitude = item["longitude"]["N"]
+                try:
+                    response = amadeus_client.safety.safety_rated_locations.get(longitude=longitude, latitude=latitude)
+                    # city_name = response.data[0]["name"]
+                    safety_scores = response.data[0]["safetyScores"] # will check if key works
+                    # might have to loop response data to make sure correct city info is getting pulled
+                    # print(response.data[0]["name"])
+                    # print(response.data[0]["safetyScores"])
+                    # print(response.data[0]["safetyScores"]["overall"])
+                    safety_data = {
+                        "safety": {"M": {"lgbtq":{"N":str(safety_scores["lgbtq"])}, "medical":{"N":str(safety_scores["medical"])}, 
+                                        "overall":{"N":str(safety_scores["overall"])}, "physical_harm":{"N":str(safety_scores["physicalHarm"])}, 
+                                        "political_freedom":{"N":str(safety_scores["politicalFreedom"])}, 
+                                        "theft":{"N":str(safety_scores["theft"])}, "women":{"N":str(safety_scores["women"])}}}
+                    }
+                    # need to check that this is correct later
+                    try:
+                        response = client.update_item(
+                            TableName="travel-destination",
+                            Key={"city": {"S": str(city)}},
+                            UpdateExpression="set #safety = :safety",
+                            ExpressionAttributeNames={
+                                "#safety": "safety",
+                            },
+                            ExpressionAttributeValues={
+                                ":safety": safety_data
+                            },
+                            ReturnValues="UPDATED_NEW",
+                        )
+                    except Exception as e:
+                        print(f"Error: {e}")
+                except ResponseError as error:
+                    print(error)
+
+
         else:
             print("No items found in the table.")
     except Exception as e:
         print(f"Error: {e}")
 
+
+    #TESTING STUFF
+
     # item structure -> { "name_of_attribute": { "data_type": data_value } }
     
-    amadeus_client = create_amadeus_client()
-    try:
-        response = amadeus_client.safety.safety_rated_locations.get(longitude=2.160873, latitude=41.397158)
-        city_name = response.data[0]["name"]
-        safety_scores = response.data[0]["safetyScores"]
-        # print(response.data[0]["name"])
-        # print(response.data[0]["safetyScores"])
-        # print(response.data[0]["safetyScores"]["overall"])
-        item_data = {
-            "city": {"S": city_name},
-            "country": {"S": "Spain"},
-            "safety": {"M": {"lgbtq":{"N":str(safety_scores["lgbtq"])}, "medical":{"N":str(safety_scores["medical"])}, 
-                             "overall":{"N":str(safety_scores["overall"])}, "physical_harm":{"N":str(safety_scores["physicalHarm"])}, 
-                             "political_freedom":{"N":str(safety_scores["politicalFreedom"])}, 
-                             "theft":{"N":str(safety_scores["theft"])}, "women":{"N":str(safety_scores["women"])}}}
-        }
-        try:
-            client.put_item(
-                TableName=table_name,
-                Item={k: v for k, v in item_data.items()}
-            )
-            print("Item created successfully.")
-        except Exception as e:
-            print(f"Error: {e}")
-    except ResponseError as error:
-        print(error)
+    # amadeus_client = create_amadeus_client()
+    # try:
+    #     response = amadeus_client.safety.safety_rated_locations.get(longitude=2.160873, latitude=41.397158)
+    #     city_name = response.data[0]["name"]
+    #     safety_scores = response.data[0]["safetyScores"]
+    #     # print(response.data[0]["name"])
+    #     # print(response.data[0]["safetyScores"])
+    #     # print(response.data[0]["safetyScores"]["overall"])
+    #     item_data = {
+    #         "city": {"S": city_name},
+    #         "country": {"S": "Spain"},
+    #         "safety": {"M": {"lgbtq":{"N":str(safety_scores["lgbtq"])}, "medical":{"N":str(safety_scores["medical"])}, 
+    #                          "overall":{"N":str(safety_scores["overall"])}, "physical_harm":{"N":str(safety_scores["physicalHarm"])}, 
+    #                          "political_freedom":{"N":str(safety_scores["politicalFreedom"])}, 
+    #                          "theft":{"N":str(safety_scores["theft"])}, "women":{"N":str(safety_scores["women"])}}}
+    #     }
+    #     try:
+    #         client.put_item(
+    #             TableName=table_name,
+    #             Item={k: v for k, v in item_data.items()}
+    #         )
+    #         print("Item created successfully.")
+    #     except Exception as e:
+    #         print(f"Error: {e}")
+    # except ResponseError as error:
+    #     print(error)
 
-    try:
-        response = client.update_item(
-            TableName="travel-destination",
-            Key={"city": {"S": "Barcelona"}},
-            UpdateExpression="set #lat = :lat, #long = :long",
-            ExpressionAttributeNames={
-                "#lat": "latitude",
-                "#long": "longitude",
-            },
-            ExpressionAttributeValues={
-                ":lat": {"N": "41.397158"},
-                ":long": {"N": "2.160873"},
-            },
-            ReturnValues="UPDATED_NEW",
-        )
-        print("Items updated successfully")
-    except Exception as e:
-        print(f"Error: {e}")
+    # try:
+    #     response = client.update_item(
+    #         TableName="travel-destination",
+    #         Key={"city": {"S": "Barcelona"}},
+    #         UpdateExpression="set #lat = :lat, #long = :long",
+    #         ExpressionAttributeNames={
+    #             "#lat": "latitude",
+    #             "#long": "longitude",
+    #         },
+    #         ExpressionAttributeValues={
+    #             ":lat": {"N": "41.397158"},
+    #             ":long": {"N": "2.160873"},
+    #         },
+    #         ReturnValues="UPDATED_NEW",
+    #     )
+    #     print("Items updated successfully")
+    # except Exception as e:
+    #     print(f"Error: {e}")
 
 
 if __name__ == "__main__":
