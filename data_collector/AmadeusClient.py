5f6b71bc6ab89f58f7537130cf9cbd445a6efc9c
Vanessa Tang <vanesst5@uci.edu>
2023-11-06 15:00:43 -0800

    Changed it so it uses production key

--- data_collector/AmadeusClient.py
+++ data_collector/AmadeusClient.py
@@ -6,19 +6,19 @@ load_dotenv()
 
 def create_amadeus_client():
     amadeus = Client(
-        client_id=os.getenv("AMADEUS_PRODUCTION_KEY"),
-        client_secret=os.getenv("AMADEUS_PRODUCTION_SECRET"),
-        host="production"
+        client_id=os.getenv("AMADEUS_PROD_KEY"),
+        client_secret=os.getenv("AMADEUS_PROD_SECRET"),
+        hostname="production"
     )
     return amadeus
 # try:
-#     response = amadeus.safety.safety_rated_locations.get(longitude=117.8265, latitude=33.6846)
+#     response = amadeus.safety.safety_rated_locations.get(longitude=-117.8259819, latitude=33.6856969)
 #     # response = amadeus.safety.safety_rated_locations.get(longitude=2.160873, latitude=41.397158)
 #     print("test")
 #     print(response.data)
-#     print(response.data[0]["name"])
-#     print(response.data[0]["safetyScores"])
-#     print(response.data[0]["safetyScores"]["overall"])
+#     # print(response.data[0]["name"])
+#     # print(response.data[0]["safetyScores"])
+#     # print(response.data[0]["safetyScores"]["overall"])
 # except ResponseError as error:
 #     print("error")
 #     print(error)
