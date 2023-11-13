ec7746e3538e79161abebe1954080075ad8a111c
Vanessa Tang <vanesst5@uci.edu>
2023-11-12 17:17:17 -0800

    Adjusted API call and how responses are dealt with ------------------------ Added radius to the API call and some city exceptions to check for when getting back a response

--- safety_test.py
+++ safety_test.py
@@ -12,6 +12,8 @@ def main():
     client = create_client(service_name="dynamodb", region_name="us-east-1")
     table_name = "travel-destination"
     amadeus_client = create_amadeus_client()
+    multiple_city = ["Muscat", "Rio de Janeiro", "Brussels", "Lisbon", "Paris"] # these cities return multiple city safety scores b/c search radius is too large
+    no_city = ["Hong Kong", "Jerusalem", "Bangkok"] # these cities do not return an overall general city safety score only city district scores
 
     try:
         response = client.scan(TableName=table_name)
@@ -23,13 +25,18 @@ def main():
                 latitude = item["latitude"]["N"]
                 longitude = item["longitude"]["N"]
                 try:
-                    response = amadeus_client.safety.safety_rated_locations.get(longitude=longitude, latitude=latitude, radius=5)
-                    # city_name = response.data[0]["name"]
-                    safety_scores = response.data[0]["safetyScores"] # will check if key works
-                    # might have to loop response data to make sure correct city info is getting pulled
-                    # print(response.data[0]["name"])
-                    # print(response.data[0]["safetyScores"])
-                    # print(response.data[0]["safetyScores"]["overall"])
+                    response = amadeus_client.safety.safety_rated_locations.get(longitude=longitude, latitude=latitude, radius=7)
+                    if city in no_city:
+                        safety_scores = response.data[0]["safetyScores"]
+                    else:
+                        for data in response.data:
+                            if data["subType"] == "CITY":
+                                if city in multiple_city:
+                                    if city == data["name"]:
+                                        safety_scores = data["safetyScores"]
+                                else:
+                                    safety_scores = data["safetyScores"]
+                                    break
                     safety_data = {"M": {"lgbtq":{"N":str(safety_scores["lgbtq"])}, 
                                          "medical":{"N":str(safety_scores["medical"])}, 
                                         "overall":{"N":str(safety_scores["overall"])}, 
@@ -38,7 +45,6 @@ def main():
                                         "theft":{"N":str(safety_scores["theft"])}, 
                                         "women":{"N":str(safety_scores["women"])}}
                                 }
-                    # need to check that this is correct later
                     try:
                         response = client.update_item(
                             TableName="travel-destination",
@@ -71,43 +77,53 @@ def main():
     
     # amadeus_client = create_amadeus_client()
     # try:
-    #     response = amadeus_client.safety.safety_rated_locations.get(longitude=2.160873, latitude=41.397158)
-    #     city_name = response.data[0]["name"]
-    #     safety_scores = response.data[0]["safetyScores"]
+    #     response = amadeus_client.safety.safety_rated_locations.get(longitude=-117.82311, latitude=33.66946, radius=7)
+    #     # city_name = response.data[0]["name"]
+    #     for data in response.data:
+    #         if data["subType"] == "CITY":
+    #             safety_scores = data["safetyScores"]
+    #             # print(safety_scores)
+    #             break
+    #     # safety_scores = response.data[0]["safetyScores"]
+    #     safety_data = {"M": {"lgbtq":{"N":str(safety_scores["lgbtq"])}, 
+    #                                     "medical":{"N":str(safety_scores["medical"])}, 
+    #                                     "overall":{"N":str(safety_scores["overall"])}, 
+    #                                     "physical_harm":{"N":str(safety_scores["physicalHarm"])}, 
+    #                                     "political_freedom":{"N":str(safety_scores["politicalFreedom"])}, 
+    #                                     "theft":{"N":str(safety_scores["theft"])}, 
+    #                                     "women":{"N":str(safety_scores["women"])}}}
     #     # print(response.data[0]["name"])
     #     # print(response.data[0]["safetyScores"])
     #     # print(response.data[0]["safetyScores"]["overall"])
-    #     item_data = {
-    #         "city": {"S": city_name},
-    #         "country": {"S": "Spain"},
-    #         "safety": {"M": {"lgbtq":{"N":str(safety_scores["lgbtq"])}, "medical":{"N":str(safety_scores["medical"])}, 
-    #                          "overall":{"N":str(safety_scores["overall"])}, "physical_harm":{"N":str(safety_scores["physicalHarm"])}, 
-    #                          "political_freedom":{"N":str(safety_scores["politicalFreedom"])}, 
-    #                          "theft":{"N":str(safety_scores["theft"])}, "women":{"N":str(safety_scores["women"])}}}
-    #     }
-    #     try:
-    #         client.put_item(
-    #             TableName=table_name,
-    #             Item={k: v for k, v in item_data.items()}
-    #         )
-    #         print("Item created successfully.")
-    #     except Exception as e:
-    #         print(f"Error: {e}")
-    # except ResponseError as error:
-    #     print(error)
+    #     # item_data = {
+    #     #     "city": {"S": city_name},
+    #     #     "country": {"S": "Spain"},
+    #     #     "safety": {"M": {"lgbtq":{"N":str(safety_scores["lgbtq"])}, "medical":{"N":str(safety_scores["medical"])}, 
+    #     #                      "overall":{"N":str(safety_scores["overall"])}, "physical_harm":{"N":str(safety_scores["physicalHarm"])}, 
+    #     #                      "political_freedom":{"N":str(safety_scores["politicalFreedom"])}, 
+    #     #                      "theft":{"N":str(safety_scores["theft"])}, "women":{"N":str(safety_scores["women"])}}}
+    #     # }
+    #     # try:
+    #     #     client.put_item(
+    #     #         TableName=table_name,
+    #     #         Item={k: v for k, v in item_data.items()}
+    #     #     )
+    #     #     print("Item created successfully.")
+    #     # except Exception as e:
+    #     #     print(f"Error: {e}")
+    # # except ResponseError as error:
+    # #     print(error)
 
-    # try:
+    # # try:
     #     response = client.update_item(
     #         TableName="travel-destination",
-    #         Key={"city": {"S": "Barcelona"}}, <--replace barcelona with city name from parse_csv results
-    #         UpdateExpression="set #lat = :lat, #long = :long", <--- replace with internet speed stuff
+    #         Key={"city": {"S": "test_city"}}, 
+    #         UpdateExpression="set #safety = :safety", 
     #         ExpressionAttributeNames={
-    #             "#lat": "latitude",
-    #             "#long": "longitude",
+    #             "#safety": "safety",
     #         },
     #         ExpressionAttributeValues={
-    #             ":lat": {"N": "41.397158"}, <----change hard coded values to internet speed from parse_csv results
-    #             ":long": {"N": "2.160873"},
+    #             ":safety": safety_data
     #         },
     #         ReturnValues="UPDATED_NEW",
     #     )
