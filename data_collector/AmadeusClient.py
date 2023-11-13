2b758d7d5aa19fe4a5235d02cc9bdc92563332a3
Vanessa Tang <vanesst5@uci.edu>
2023-11-12 17:15:45 -0800

    Removed extra comments

--- data_collector/AmadeusClient.py
+++ data_collector/AmadeusClient.py
@@ -11,17 +11,3 @@ def create_amadeus_client():
         hostname="production"
     )
     return amadeus
-
-# if __name__ == "__main__":
-#     amadeus = create_amadeus_client()
-#     try:
-#         response = amadeus.safety.safety_rated_locations.get(longitude=72.878176, latitude=19.0785451, radius=5)
-#         # response = amadeus.safety.safety_rated_locations.get(longitude=2.160873, latitude=41.397158)
-#         print("test")
-#         print(response.data)
-#         # print(response.data[0]["name"])
-#         # print(response.data[0]["safetyScores"])
-#         # print(response.data[0]["safetyScores"]["overall"])
-#     except ResponseError as error:
-#         print("error")
-#         print(error)
